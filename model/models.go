fb1d6564b4e8781da9fbf79f43df8b83a0403550
Mustafa Khan <mustafa.khan@datamato.com>
2024-04-26 10:14:12 +0530

    feat: Implement CRUD operations for users table

--- model/models.go
+++ model/models.go
@@ -5,7 +5,6 @@ import (
 )
 
 // fields of the table theatre from mongodb
-
 type Theatre struct {
 	ID      primitive.ObjectID `json:"_id,omitemppty" bson:"_id,omitempty"`
 	Watched bool               `json:"watched,omitempty"`
